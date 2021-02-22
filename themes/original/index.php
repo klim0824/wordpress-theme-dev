2575c6c776b72dea7e7aa4a21ad897973f0bf840
klim0824 <klim0824@live.jp>
2021-02-23 01:33:19 +0900

    update metadata

--- themes/original/index.php
+++ themes/original/index.php
@@ -1,5 +1,31 @@
-<?php get_header(); ?>
-
-<?php get_template_part( 'template-parts/loop' ); ?>
-
-<?php get_footer();
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title><?php wp_title( '|', true, 'right' ); bloginfo( 'name' ); ?></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="format-detection" content="telephone=no">
+	<meta name="description" content="<?php bloginfo( 'description' ); ?>">
+	<meta property="og:type" content="website">
+    <meta property="og:title" content="<?php wp_title( '|', true, 'right' ); bloginfo( 'name' ); ?>">
+    <meta property="og:description" content="<?php bloginfo( 'description' ); ?>">
+    <meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/assets/images/ogp.jpg">
+    <meta property="og:local" content="<?php bloginfo( 'language' ); ?>">
+    <meta property="og:url" content="<?php echo get_pagenum_link(get_query_var('page')); ?>">
+    <meta property="og:site_name" content="<?php bloginfo( 'name' ); ?>">
+	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.png">
+	<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/apple-touch-icon.png">
+	<link rel="alternate" href="<?php bloginfo( 'rss2_url' ); ?>" type="application/rss+xml" title="RSS">
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<noscript>このサイトはJavaScriptを使用しております。</noscript>
+<?php wp_body_open(); ?>
+<main role="main" id="main">
+<?php date_default_timezone_set ('Asia/Tokyo'); ?>
+<?php echo date('Y-m-d H:i:s'); ?>
+</main>
+<?php wp_footer(); ?>
+</body>
+</html>
