201285ed3325e536cd21cd9d0bc39488e6f2495f
klim0824 <klim0824@live.jp>
2021-04-06 22:40:16 +0900

    update single page

--- themes/original/single.php
+++ themes/original/single.php
@@ -10,15 +10,14 @@
 	<?php get_template_part( 'template-parts/header' ); ?>
 
 	<main role="main" id="main">
-		<?php if ( have_posts() ) : ?>
-			<ul>
-				<?php while ( have_posts() ) : the_post(); ?>
-					<li>
-						<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-					</li>
-				<?php endwhile; ?>
-			</ul>
-		<?php endif; ?>
+		<?php if ( have_posts() ) :?>
+			<?php while ( have_posts() ) : the_post(); ?>
+				<h2><?php the_title(); ?></h2>
+				<div class="">
+					<?php the_content(); ?>
+				</div>
+			<?php endwhile?>
+		<?php endif;?>
 	</main>
 	<?php get_template_part( 'template-parts/footer' ); ?>
 	<?php wp_footer(); ?>
