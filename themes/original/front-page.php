edb177f3ca43f7301f57f1d710fb98f5a9a1336e
klim0824 <klim0824@live.jp>
2021-04-11 23:00:34 +0900

    display an articles list with WP_Query

--- themes/original/front-page.php
+++ themes/original/front-page.php
@@ -16,14 +16,27 @@
 		</section>
 		<section>
 			<h2>NEWS</h2>
-			<?php if ( have_posts() ) : ?>
+			<?php
+				$args = [
+					'category_name' => 'news',
+					'orderby' => 'post_date',
+					'order' => 'DESC',
+				];
+				$query = new WP_Query( $args );
+			?>
+			<?php if ( $query->have_posts() ) : ?>
 				<ul>
-					<?php while ( have_posts() ) : the_post(); ?>
+					<?php while ( $query->have_posts() ) : $query->the_post(); ?>
 						<li>
-							<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+							<a href="<?php the_permalink(); ?>">
+								<?php the_title(); ?>
+							</a>
 						</li>
 					<?php endwhile; ?>
 				</ul>
+				<?php wp_reset_postdata(); ?>
+			<?php else : ?>
+				<p>まだ投稿がありません。</p>
 			<?php endif; ?>
 		</section>
 		<section>
